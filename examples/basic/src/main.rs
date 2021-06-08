b0ba8b87c0364cb6cc5faefbf01ff301758b8472
A_Hibi <bb.is.what.he.believes@gmail.com>
2021-06-09 00:36:52 +0900

    update example

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -1,11 +1,10 @@
-use metron_core::def_unit_measure;
-def_unit_measure!(some_unit::SomeUnit);
+metron_core::def_unit_measure!(some_unit::SomeUnit);
 fn main() {
     use some_unit::SomeUnit;
     let lhs = SomeUnit::of(4);
-    let rhs = SomeUnit::of(3);
+    let rhs = SomeUnit::from(3);
     assert_eq!(&7, (lhs + rhs).num());
     assert_eq!(1, (lhs - rhs).into_num());
-    assert_eq!(SomeUnit::of(8), lhs * 2);
-    assert_eq!(true, lhs / 2 < rhs);
+    assert_eq!(SomeUnit::new(8), lhs * 2);
+    assert!(lhs / 2 < rhs);
 }
